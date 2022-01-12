054e9c6ea4de13cdf605e580f21de7851f8ecb31
JackLais <jlais@bsd220.org>
2022-01-12 10:41:31 -0600

    working due dates

--- To Do List Hackwich/ContentView.swift
+++ To Do List Hackwich/ContentView.swift
@@ -9,16 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var toDoItems =
-            [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
-             ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
-             ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
-
+        [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
+         ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
+         ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
+    
     var body: some View {
         NavigationView {
             List {
-                ForEach (toDoItems) {
-                    item in
-                    Text(item.description)
+                ForEach (toDoItems) { item in
+                    VStack {
+                        Text(item.priority)
+                            .font(.headline)
+                        Text(item.description)
+                    }
+                    Spacer()
+                    Text(item.dueDate, style: .date)
                 }
                 .onMove(perform: { indices, newOffset in
                     toDoItems.move(fromOffsets: indices, toOffset: newOffset)
